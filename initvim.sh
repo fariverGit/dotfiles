49fb144d88b57dad74390759adc1ba779b5b58fb
Daniel D <djx339@gmail.com>
2014-09-21 23:23:35 +0800

    Add install basic soft

--- initvim.sh
+++ initvim.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+#
+# Initial linux env
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# sudo without password
+echo "$USER ALL=(ALL) NOPASSWD :ALL" >> /etc/sudoers
+
+# Install basic soft
+sudo add-apt-repository ppa:fcwu-tw/ppa
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install -y aptitude build-essential linux-headers-$(uname -r) python-dev cmake git vim openssh-server
+
+[[ -d .vim/bundle/Vundle.vim ]] \
+  || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+./.vim/bundle/YouCompleteMe/install.sh
