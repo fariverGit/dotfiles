4fe571b34c60fa8f8315407346d0d485032c3de8
Daniel D <djx339@gmail.com>
2015-10-01 16:51:31 +0800

    fist install patched fonts

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@ $!/bin/bash
 
 _dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# install patched fonts
+ln -s "$_dir"/fonts ~/.fonts
+"$_dir"/fonts/install.sh
+
+# setup vim
 ln -s "$_dir"/vim/vimrc ~/.vimrc
 [[ -d "$_dir"/vim/bundle/Vundle.vim ]] \
     || git clone https://github.com/gmarik/Vundle.vim.git "$_dir"/vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 "$_dir"/vim/bundle/YouCompleteMe/install.py
 
-ln -s "$_dir"/fonts ~/.fonts
-"$_dir"/fonts/install.sh
