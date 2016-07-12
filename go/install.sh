c2185887a23bb453f7b7c37030dee5d53e70562d
Daniel D <djx339@gmail.com>
2016-07-12 13:14:56 +0800

    Update install.sh

--- go/install.sh
+++ go/install.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
 
+set -e
+
+apt-get update && apt-get install -y --no-install-recommends \
+		g++ \
+		gcc \
+		libc6-dev \
+		make
+
+GOLANG_VERSION=1.6.2
+GOLANG_DOWNLOAD_URL=https://golang.org/dl/go$GOLANG_VERSION.linux-amd64.tar.gz
+GOLANG_DOWNLOAD_SHA256=e40c36ae71756198478624ed1bb4ce17597b3c19d243f3f0899bb5740d56212a
+
+curl -fsSL "$GOLANG_DOWNLOAD_URL" -o golang.tar.gz \
+	&& echo "$GOLANG_DOWNLOAD_SHA256  golang.tar.gz" | sha256sum -c - \
+	&& tar -C /usr/local -xzf golang.tar.gz \
+	&& rm golang.tar.gz
+
+# export GOPATH=/work/go
+# export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
